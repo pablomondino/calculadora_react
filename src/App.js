0405913feb3afd8ad875a4a7273036b91d055f29
Pablo Mondino De La Rossa <p_mondino@hotmail.com>
2022-06-27 22:56:16 -0300

    arme botones y pantalla

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import logoCalculadora from './imagenes/logo-calculadora.png';
 
 import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
 
 function App() {
   return (
@@ -16,10 +17,36 @@ function App() {
       />
     </div>
     <div className='contenedor-calculadora'>
-      <div className='fila'><Boton>1</Boton> <Boton>+</Boton></div>
-      <div className='fila'>2</div>
-      <div className='fila'>3</div>
-      <div className='fila'>fila 4</div>
+      <Pantalla />
+      
+      <div className='fila'>
+        <Boton>1</Boton>
+        <Boton>2</Boton>
+        <Boton>3</Boton>
+        <Boton>+</Boton>
+      </div>
+      <div className='fila'>
+        <Boton>4</Boton>
+        <Boton>5</Boton>
+        <Boton>6</Boton>
+        <Boton>-</Boton>
+        </div>
+      <div className='fila'>
+        <Boton>7</Boton>
+        <Boton>8</Boton>
+        <Boton>9</Boton>
+        <Boton>*</Boton>
+      </div>
+      <div className='fila'>
+        <Boton>=</Boton>
+        <Boton>0</Boton>
+        <Boton>.</Boton>
+        <Boton>/</Boton>
+      </div>
+     
+     
+     
+     
       <div className='fila'>clear</div>
 
 
